7fa82f42d85a7adc70ce498ef3a8e34307fa121a
Guilherme Galli <Galli.Guilherme@senior.com.br>
2026-09-04 19:31:20 -0300

    Ajustes Login

--- supabase/migrations/017_codigo_usuario_4_ou_6_digitos.sql
+++
@@ -1,4 +0,0 @@
--- Permite código de sócio com 4 dígitos (titular/dependente) ou visitante com 6.
-ALTER TABLE usuarios DROP CONSTRAINT IF EXISTS usuarios_codigo_usuario_check;
-ALTER TABLE usuarios ADD CONSTRAINT usuarios_codigo_usuario_check
-  CHECK (codigo_usuario ~ '^\d{4}$' OR codigo_usuario ~ '^\d{6}$');
